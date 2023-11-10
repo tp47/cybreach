4a38f3f28ccdae6f0f02f41637aa3a91ca611ce9
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-11-10 08:25:09 +0300

    CYB-28, Настроить path alias в tsconfig и vite, add path alias

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,9 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
+});
