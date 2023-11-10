6287ba1ec6f739f526eb6f07ec96f2c6ae857f24
tp47 <116389510+tp47@users.noreply.github.com>
2023-11-10 08:45:29 +0300

    CYB-28, Настроить path alias в tsconfig и vite (#2)
    * CYB-28, Настроить path alias в tsconfig и vite, add path alias
    
    * CYB-28, Настроить path alias в tsconfig и vite, format entire project
    
    ---------
    
    Co-authored-by: Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
