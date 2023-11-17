f5adef41a090c3616742abb1cd6fca7214f05b0c
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-11-17 11:14:19 +0300

    CYB-26, Разработать механику игры на Canvas API, fix pr thread

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -1,28 +1,8 @@
-import { Game } from "@/model";
-import { useEffect, useRef } from "react";
-
-function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    if (canvasRef.current === null) {
-      return;
-    }
-    const game = new Game(canvasRef.current, {
-      seed: Math.random().toString(),
-      level: 12,
-    });
-
-    return () => {
-      game.destruct();
-    };
-  }, []);
-  return <canvas ref={canvasRef}></canvas>;
-
-/* import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "../router";
 
 function App() {
-  return <RouterProvider router={router} />; */
+  return <RouterProvider router={router} />;
 }
 
 export default App;
