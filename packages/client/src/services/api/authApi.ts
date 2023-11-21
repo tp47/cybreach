3a01fadefa3e8732d96b65bc0b7b31bf30cd06c0
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-11-21 04:59:42 +0300

    refactor : type User is imported now

--- packages/client/src/services/api/authApi.ts
+++ packages/client/src/services/api/authApi.ts
@@ -1,14 +1,5 @@
 import { BASE_URL } from '@/constants/baseUrl'
-
-type User = {
-  first_name: string
-  second_name: string
-  display_name?: string
-  login: string
-  email: string
-  password: string
-  phone: string
-}
+import { User } from '@/types'
 
 type ResponseData = {
   message?: string
