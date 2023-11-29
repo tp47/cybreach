971a9db0e8610b2b98e1db494a0736b38f03d608
Gleb Pologov <gpologov@gmail.com>
2023-11-29 12:54:08 +0300

    CYB-38

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,6 +5,23 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/configure'
 
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope)
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error)
+        })
+    })
+  }
+}
+
+startServiceWorker()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
