ac21cc8d6eaa3e0b971d742e59f9b59429c4d9ff
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-12-01 03:02:32 +0300

    fix: css

--- packages/client/src/components/organisms/EndGameScreen/EndGameScreen.tsx
+++ packages/client/src/components/organisms/EndGameScreen/EndGameScreen.tsx
@@ -9,7 +9,7 @@ type EndGameScreenProps = {
 
 export default function EndGameScreen({ onStartGame, onLeaveGame, result }: EndGameScreenProps) {
   return (
-    <section className="flex flex-col h-full w-full border-2 border-green-300 rounded-2xl p-[60px] bg-right bg-contain gap-[24px] justify-center items-center bg-custom-game">
+    <section className="flex flex-col h-full w-full border-2 border-green-300 rounded-2xl p-[60px] bg-right gap-[24px] justify-center items-center bg-custom-game bg-no-repeat bg-cover">
       {result === GameResult.SOLVED && (
         <div className="text-green-300 text-3xl w-[60%] uppercase text-center mb-10">
           you solved the code matrix!
